8bb8dd361fd77a1ca9f57466dcd0086d25169f4e
Tafari <tafarigriffith2016@gmail.com>
2026-05-24 14:23:02 -0600

    Sets up Layers/Masks to control what object can see for collisions; Changes car movement to use delta

--- scripts/car.lua
+++ scripts/car.lua
@@ -1,26 +1,29 @@
 local car = {
 	extends = Area2D,
 	direction = Vector2.LEFT,
-	speed = 2
+	speed = 200
 }
 
 
-
-
 function car:_ready()
-
 	if self.position.x < 192 then
 		self.direction = Vector2.RIGHT
 		self.flip_h = true
 	end
+
+	return
 end
 
-function car:_process(delta)
-	self.position = self.position + (self.direction * self.speed)
+function car:_physics_process(delta)
+	self.position = self.position + (self.direction * self.speed * delta)
+
+	return
 end
 
 function car:_on_visible_on_screen_notifier_2d_screen_exited()
 	self:queue_free()
+
+	return
 end
 
 return car
--- scripts/game.lua
+++ scripts/game.lua
@@ -2,7 +2,9 @@ local game = {
 	extends = Node2D,
 }
 
-
+function go_to_title(body)
+	print(body)
+end
 
 function game:_ready()
 	car_Scene = ResourceLoader:load("res://scenes/car.tscn")
@@ -15,8 +17,9 @@ function game:_on_car_timer_timeout()
 	local car = car_Scene:instantiate()
 	local position_Marker = car_Start_Positions:get_children():pick_random()
 	car.position = position_Marker.position
-	
+
 	Objects:add_child(car)
+	car:connect("body_entered", Callable(go_to_title))
 end
 
 
