10dd66c788082494d738726483af5b08694e3794
Tafari <tafarigriffith2016@gmail.com>
2026-05-24 20:51:56 -0600

    Adds Random Colour Selection to moving cars; Creates more obstacles for player

--- scripts/car.lua
+++ scripts/car.lua
@@ -1,15 +1,23 @@
 local car = {
 	extends = Area2D,
 	direction = Vector2.LEFT,
-	speed = 200
+	speed = 200,
+	colours = {
+		ResourceLoader:load("res://graphics/cars/green.png"),
+		ResourceLoader:load("res://graphics/cars/red.png"),
+		ResourceLoader:load("res://graphics/cars/yellow.png")
+	}
 }
-
 
 function car:_ready()
 	if self.position.x < 192 then
 		self.direction = Vector2.RIGHT
 		self.flip_h = true
 	end
+
+
+	local sprite = self:get_node("Sprite2D")
+	sprite.texture = self.colours[math.random(#self.colours)]
 
 	return
 end
